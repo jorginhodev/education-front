78cc275649d32016608727c57ab00b61f5113888
Jorge Ramos <jorge.ramos@atlastechnol.com>
2020-10-21 16:25:43 -0300

    :recycle: Added font Roboto

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,12 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 </style>
